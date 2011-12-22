abf42def5f03f8097128b418d707d0466d49030a
Benito Rodríguez Arcos <brarcos@gmail.com>
2011-12-22 07:52:36 +0000

    mas cambios

---
+++ core/beruby.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import config
+
+# http://doc.scrapy.org/en/latest/intro/overview.html
+
+class BeRuby(object):
+    def __init__(self, plugins):
+        self._plugins = plugins
+        
+        # self.plugins[0].plugin_object.print_name()
+        
+    def login(self):
+        pass
+        
